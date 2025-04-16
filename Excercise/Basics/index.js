f9f4830254e52c305e01190d2801f193c1be07d3
Divika S <67966542+divi7204@users.noreply.github.com>
2025-04-16 20:31:22 +0530

    Create index.js

---
+++ Excercise/Basics/index.js
@@ -0,0 +1,23 @@
+
+console.log("This is inside the index.js file");
+
+
+//how to declare a object
+
+let person = {
+    name: 'Divika',
+    age : 23
+}
+
+console.log(person)
+
+//how to access the value of a property
+// using dot operator
+// using the square brakets
+person.name =  "divika";
+console.log(person.name);
+
+person["age"] = 24;
+console.log(person["age"]);
+
+
